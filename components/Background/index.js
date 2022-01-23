d2f06b553fc53f83761b20449fdb8f515300dfb2
Hope Kumordzie <officialhopek42@gmail.com>
2022-01-23 09:56:24 -0600

    fouc test

---
+++ components/Background/index.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import styles from '../../styles/background.module.scss'
+
+const Background = () => {
+    return (
+        <>
+            <div className={styles.background}></div>
+        </>
+    )
+};
+
+export default Background;
