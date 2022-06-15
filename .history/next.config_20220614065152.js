0cc42245a72e34cdd712938c93731c79e39ecce4
hope-k <hopekumordzie@gmail.com>
2022-06-16 07:51:19 +0800

    changed scroll behaviour

--- .history/next.config_20220614065152.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com']
-  },
-  env: {
-    SENDGRID_API_KEY: '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'
-  }
-}
